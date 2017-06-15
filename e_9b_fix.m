ecff4ab60e331ad4a6ec956e385c29d67d31fb82
goodengineer <gutierrez_nacho@hotmail.com>
2017-06-15 17:37:33 -0300

    fix 9b extended

--- e_9b_fix.m
+++ e_9b_fix.m
@@ -1,6 +1,14 @@
-function e_9b()
+function [model reg] = e_9b()
     load hald;
 
     % Como se interpretan estos resultados?
-    [b,se,pval,inmodel,stats,nextstep,history] = stepwisefit(ingredients, heat)
+    [b,se,pval,inmodel,stats,nextstep,history] = stepwisefit(ingredients, heat);
+
+
+    model = [stats.intercept; b(find(inmodel>0))];
+    ing2 = ingredients(:,find(inmodel>0));
+    ing2 = [ones(size(ing2,1), 1) ing2];
+
+    % reg should be close to heat
+    reg = ing2*model;
 end
